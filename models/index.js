3088286946c913ac497c63f56ed52063571d667c
ogMav8 <brad.anderson019@gmail.com>
2021-07-19 17:48:40 -0600

    added models and associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'Products_tags',
+  foreignKey: 'Product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'Product_tags',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
